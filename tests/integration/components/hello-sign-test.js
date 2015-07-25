7d25f13a3c5422aef1c491b59d4573deca23aa15
Qi He <qhe@heyook.com>
2015-07-25 09:56:59 -0700

    add throw error test

--- tests/integration/components/hello-sign-test.js
+++ tests/integration/components/hello-sign-test.js
@@ -5,14 +5,26 @@ moduleForComponent('hello-sign', 'Integration | Component | hello sign', {
   integration: true
 });
 
+test('it throws error if Signing URL is not provided', function(assert) {
+  var self  = this;
+  var error = [
+    "SignUrl must be set to use the hello-sign component. You can set the ",
+    "key property on the component when instantiating it in your hbs template. ",
+    "See how to get SignUrl at https://www.hellosign.com/home/myAccount#api"
+  ].join('\n');
+
+  assert.throws(function() {
+    self.render(hbs`{{hello-sign}}`);
+  }, error, "has thrown an Error");
+});
+
 test('it renders hellosign embedded frame', function(assert) {
   assert.expect(0);
 
   // TODO: how to wait for third party JS to complete
   // this.set('url', "https://www.hellosign.com/editor/embeddedSign");
-  //
   // this.render(hbs`{{hello-sign url=url}}`);
-  // 
+  //
   // assert.equal(this.$("#hsEmbeddedOverlay").length, 1);
 });
 
@@ -21,7 +33,6 @@ test('it sends onEventInvalid action', function(assert) {
 
   // TODO: how to wait for third party JS to complete
   // this.set('url', "https://www.hellosign.com/editor/embeddedSign");
-  //
   // this.on('onEventInvalid', function() {
   //   assert.ok(true);
   // });
